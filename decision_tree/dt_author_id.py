ca73d22a8473a1de4cb2c1b45a02516d1897600f
Peter Griffin <peterhughgriffin@gmail.com>
2020-06-06 17:09:57 +0100

    End of Lesson 4 DT

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+Reduce=1
+
+features_train_red = features_train[:int(len(features_train)/Reduce)]
+labels_train_red = labels_train[:int(len(labels_train)/Reduce)]
+
+t0 = time()
+clf.fit(features_train_red, labels_train_red)
+print ("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print ("prediction time:", round(time()-t1, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+Accuracy = accuracy_score(labels_test, pred)
+
+print(Accuracy)
 
 #########################################################
 
