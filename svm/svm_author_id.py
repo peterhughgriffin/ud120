b218639eff18d707dc0332367ea22609f882820c
Peter Griffin <peterhughgriffin@gmail.com>
2020-06-06 15:09:42 +0100

    End of Lesson 3 SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,8 +22,45 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-#########################################################
+#%% ########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+
+clf = SVC(C=10000.0,kernel="rbf")
+
+Reduce=1
+
+features_train_red = features_train[:int(len(features_train)/Reduce)]
+labels_train_red = labels_train[:int(len(labels_train)/Reduce)]
+
+t0 = time()
+clf.fit(features_train_red, labels_train_red)
+print ("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print ("prediction time:", round(time()-t1, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+Accuracy = accuracy_score(labels_test, pred)
+
+print(Accuracy)
+#%% 
+
+# print(clf.predict([features_test[50]]))
+Count0=0
+Count1=0
+for item in pred:
+    if item == 0:
+        Count0=Count0+1
+    else:
+        Count1=Count1+1
+
+print(Count1)
+
+
 
 #########################################################
 
